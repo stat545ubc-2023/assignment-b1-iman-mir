113aef48be7ae7509c0ff5cc28abae05cce15cfc
Iman Mir <imanmir@student.ubc.ca>
2023-11-01 12:49:54 -0700

    counter in man file

--- counter/R/counter.R
+++ counter/R/counter.R
@@ -1,3 +1,14 @@
+#' Counts Data Points in Data Frame Group
+#'
+#' @param df A dataframe with different groups.
+#' @param group_var The group of interest.
+#'
+#' @return A tibble.
+#' @export
+#'
+#' @examples
+#' counter(vancouver_trees, height_range_id)
+#' counter(vancouver_trees, cultivar_name )
 counter <- function(df, group_var) {
      df |>
     group_by({{ group_var }}) |>
